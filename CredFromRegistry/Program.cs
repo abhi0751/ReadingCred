afd985d86f92fd8d5fe15fb003f9d4883df1a227
Abhi0751 <abhi0751@gmail.com>
2025-07-04 06:36:57 +0530

    initial check in
    initial check in

---
+++ CredFromRegistry/Program.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Win32;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+
+namespace CredFromRegistry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("creating key at  Hkey/currentuser");
+            string keylocation = @"Shukla\ShuklaApp";
+            string resitrykey = "appid";
+            string registryvalue = "demoApp";
+
+            WriteInWinRegistry(keylocation, resitrykey, registryvalue);
+
+             string abc = ReadFromWinRegitry(keylocation, resitrykey);
+             Console.WriteLine($"reading Registry value {abc}");
+
+             Console.WriteLine($"Trying Reading non existing Registry location :  {ReadFromWinRegitry(@"Shukla\ShuklaApp1", "appid123")}");
+
+
+        }
+
+
+
+        private static void WriteInWinRegistry(string RegistoryLocation, string RegistryKey,string RegistryKeyValue)
+        {
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(RegistoryLocation);
+            key.SetValue(RegistryKey, RegistryKeyValue);
+            key.Close();
+        }
+
+
+        private static string ReadFromWinRegitry(string RegistoryLocation, string RegistryKey)
+        { 
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(RegistoryLocation);
+
+            string keyvalue = "Registry location not exists. ";
+            
+            if (key != null)
+            {
+               keyvalue = (string)key.GetValue(RegistryKey);
+                if(keyvalue == null)
+                {
+                    keyvalue = "Key either not exists or has null value";
+                }
+                
+            }
+            
+            return keyvalue;
+
+        }
+
+    }
+
+
+
+
+
+
+
+}
---
+++ CredFromWinCredManager/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System.ComponentModel;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using System.Text;
+
+
+
+
+internal partial class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string appName = "ShuklaApp";
+        string userName = "user123";
+        string password = "P@ssw0rd!";
+        string comment = "User login information";
+
+        CredentialManager123.WriteCredential(appName, userName, password);
+
+        Console.WriteLine("Credential saved successfully.");
+
+        
+
+
+            var credential = CredentialManager123.ReadCredential(appName);
+        if (credential == null)
+        {
+            Console.WriteLine("No credential found.");
+            
+        }
+
+        Console.WriteLine($"UserName: {credential.UserName}");
+        Console.WriteLine($"Secret: {credential.Password}");
+       
+
+    }
+}
+
+    
+
